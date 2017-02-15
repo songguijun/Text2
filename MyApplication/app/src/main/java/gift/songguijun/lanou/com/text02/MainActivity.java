41fad08cb94f326be7fdbfd168bf319adf8ebe75
GuiJunEr <1106681822@qq.com>
2017-02-15 14:16:09 +0800

    练习2

---
+++ MyApplication/app/src/main/java/gift/songguijun/lanou/com/text02/MainActivity.java
@@ -0,0 +1,13 @@
+package gift.songguijun.lanou.com.text02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
