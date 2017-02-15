9cd35b97ab6502a2e0d4ba283ff0e5e9960c47ef
Lin <291587302@qq.com>
2017-02-15 14:45:50 +0800

    登录功能删除

--- MyApplication/app/src/main/java/gift/songguijun/lanou/com/text02/MainActivity.java
+++ MyApplication/app/src/main/java/gift/songguijun/lanou/com/text02/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //登录功能完成
+        //登录功能让我删了
     }
 }
---
+++ MyApplication/app/src/main/java/gift/songguijun/lanou/com/text02/MyBean.java
@@ -0,0 +1,8 @@
+package gift.songguijun.lanou.com.text02;
+
+/**
+ * Created by dllo on 17/2/15.
+ */
+
+public class MyBean {
+}
